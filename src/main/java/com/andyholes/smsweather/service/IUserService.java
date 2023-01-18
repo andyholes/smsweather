2418e7e9e7ed859812fcfece9519fdedeb71137f
andyholes <andres.hoyos202761@alumnos.info.unlp.edu.ar>
2023-01-18 18:10:30 -0300

    update service methods

--- src/main/java/com/andyholes/smsweather/service/IUserService.java
+++ src/main/java/com/andyholes/smsweather/service/IUserService.java
@@ -2,6 +2,7 @@ package com.andyholes.smsweather.service;
 
 import com.andyholes.smsweather.model.UserEntity;
 import com.andyholes.smsweather.model.dto.UserDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +10,15 @@ import java.util.List;
 @Service
 public interface IUserService {
 
-    UserDto saveUser(UserEntity user);
+    UserDto saveUser(UserDto dto);
 
-    UserDto updateUser(UserEntity user);
+    UserDto updateUser(UserDto dto);
 
-    boolean validateUser (String code);
+    boolean validateUser (String phone, String code);
 
-    boolean deactivateUser (String phone);
+    boolean deactivateUser (String phone, String code);
 
     List<UserDto> getAllUsers();
+    
+    String generateCode();
 }
