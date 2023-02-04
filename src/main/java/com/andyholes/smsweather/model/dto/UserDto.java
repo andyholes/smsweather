69e54ad7da446711633e6b2faf15e6b061bef5f2
andyholes <andyholesdev@gmail.com>
2023-02-03 21:33:36 -0300

    add constructor to dto

--- src/main/java/com/andyholes/smsweather/model/dto/UserDto.java
+++ src/main/java/com/andyholes/smsweather/model/dto/UserDto.java
@@ -2,9 +2,13 @@ package com.andyholes.smsweather.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
