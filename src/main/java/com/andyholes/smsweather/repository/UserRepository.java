192a599d1d32d07fe737e97e86aa80d799423e69
Andres Hoyos Garcia <andyholesdev@gmail.com>
2023-02-15 13:38:23 -0300

    update id type on repository

--- src/main/java/com/andyholes/smsweather/repository/UserRepository.java
+++ src/main/java/com/andyholes/smsweather/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface UserRepository extends MongoRepository<UserEntity, Long> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
 
     UserEntity findByPhone (String phone);
 
